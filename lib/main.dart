051d22450561b233cbd13954239f79ca12df753e
dan12411 <dan12411@ecrowdmedia.com>
2021-07-05 09:38:40 +0800

    refactor: use bloc replace setState

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-import 'dart:convert';
-
+import 'package:best_architecture_challenge/features/fetch_posts/presentation/bloc/fetch_posts_bloc.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'injection_container.dart' as di;
 
@@ -20,37 +19,26 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home: PostPage(title: 'FlutterTaipei :)'),
+      home: BlocProvider(
+        create: (_) => di.sl<FetchPostsBloc>()..add(StartFetchPosts()),
+        child: PostPage(title: 'FlutterTaipei :)'),
+      ),
     );
   }
 }
 
-class PostPage extends StatefulWidget {
+class PostPage extends StatelessWidget {
   PostPage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _PostPageState createState() => _PostPageState();
-}
-
-class _PostPageState extends State<PostPage> {
   static const int _sortWithId = 1;
   static const int _sortWithTitle = 2;
 
-  List<dynamic> _posts = [];
-
-  @override
-  void initState() {
-    super.initState();
-    _fetchData(_sortWithId);
-  }
+  final String title;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text(widget.title),
+          title: Text(title),
           actions: <Widget>[
             PopupMenuButton(
                 icon: Icon(Icons.more_vert),
@@ -65,57 +53,65 @@ class _PostPageState extends State<PostPage> {
                       )
                     ],
                 onSelected: (int value) {
-                  _fetchData(value);
+                  BlocProvider.of<FetchPostsBloc>(context)
+                      .add(StartFetchPosts());
                 })
           ],
         ),
-        body: ListView.separated(
-          itemCount: _posts.length,
-          itemBuilder: (context, index) {
-            String id = _posts[index]['id'].toString();
-            String title = _posts[index]['title'].toString();
-            String body = _posts[index]['body'].toString();
-            return Container(
-                padding: EdgeInsets.all(8),
-                child: RichText(
-                  text: TextSpan(
-                    style: DefaultTextStyle.of(context).style,
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: "$id. $title",
-                        style: TextStyle(fontSize: 18, color: Colors.red),
-                      ),
-                      TextSpan(
-                        text: '\n' + body,
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  ),
-                ));
-          },
-          separatorBuilder: (context, index) {
-            return Divider();
-          },
-        ));
+        body: BlocBuilder<FetchPostsBloc, FetchPostsState>(
+            builder: (context, state) {
+          if (state is FetchPostsLoaded) {
+            final posts = state.posts;
+            return ListView.separated(
+                itemCount: posts.length,
+                itemBuilder: (context, index) {
+                  String id = posts[index].id;
+                  String title = posts[index].title;
+                  String body = posts[index].body;
+                  return Container(
+                      padding: EdgeInsets.all(8),
+                      child: RichText(
+                        text: TextSpan(
+                          style: DefaultTextStyle.of(context).style,
+                          children: <TextSpan>[
+                            TextSpan(
+                              text: "$id. $title",
+                              style: TextStyle(fontSize: 18, color: Colors.red),
+                            ),
+                            TextSpan(
+                              text: '\n' + body,
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ));
+                },
+                separatorBuilder: (context, index) {
+                  return Divider();
+                });
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        }));
   }
 
-  void _fetchData(int sort) async {
-    var url = Uri.https('jsonplaceholder.typicode.com', '/posts');
-    var response = await http.get(url);
-    print("response=${response.body}");
-    List<dynamic> result = jsonDecode(response.body);
-    if (sort == _sortWithId) {
-      result.sort((a, b) {
-        return int.parse(a['id'].toString())
-            .compareTo(int.parse(b['id'].toString()));
-      });
-    } else if (sort == _sortWithTitle) {
-      result.sort((a, b) {
-        return a['title'].toString().compareTo(b['title'].toString());
-      });
-    }
-    setState(() {
-      _posts = result;
-    });
-  }
+// void _fetchData(int sort) async {
+//   var url = Uri.https('jsonplaceholder.typicode.com', '/posts');
+//   var response = await http.get(url);
+//   print("response=${response.body}");
+//   List<dynamic> result = jsonDecode(response.body);
+//   if (sort == _sortWithId) {
+//     result.sort((a, b) {
+//       return int.parse(a['id'].toString())
+//           .compareTo(int.parse(b['id'].toString()));
+//     });
+//   } else if (sort == _sortWithTitle) {
+//     result.sort((a, b) {
+//       return a['title'].toString().compareTo(b['title'].toString());
+//     });
+//   }
+//   setState(() {
+//     _posts = result;
+//   });
+// }
 }
